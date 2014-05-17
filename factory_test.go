7f6457c5f2941c8620a1b5ead779b4adb0ce6ca1
Vladimir Sibirov <trustmaster@kodigy.com>
2014-05-17 22:48:29 +0400

    Added a test for creating subgraphs as factory components at run-time.

--- factory_test.go
+++ factory_test.go
@@ -57,3 +57,50 @@ func TestFactoryConnection(t *testing.T) {
 		t.Errorf("Error: %d != 123", i)
 	}
 }
+
+// Creates a graph that will be loaded at run-time
+func newDummyNet() interface{} {
+	net := new(dummyNet)
+	net.InitGraphState()
+
+	net.AddNew("echoer", "e1")
+	net.AddNew("echoer", "e2")
+
+	net.Connect("e1", "Out", "e2", "In")
+
+	net.MapInPort("In", "e1", "In")
+	net.MapOutPort("Out", "e2", "Out")
+
+	return net
+}
+
+func init() {
+	Register("DummyNet", newDummyNet)
+}
+
+// Tests adding subgraph components at run-time
+func TestFactorySubgraph(t *testing.T) {
+	net := new(dummyNet)
+	net.InitGraphState()
+
+	net.AddNew("DummyNet", "d1")
+
+	net.MapInPort("In", "d1", "In")
+	net.MapOutPort("Out", "d1", "Out")
+
+	in, out := make(chan int), make(chan int)
+
+	net.SetInPort("In", in)
+	net.SetOutPort("Out", out)
+
+	RunNet(net)
+
+	in <- 123
+	i := <-out
+
+	close(in)
+
+	if i != 123 {
+		t.Errorf("Error: %d != 123", i)
+	}
+}
