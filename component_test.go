252b3c6a8a49b2d9457ae9e2c335970aef2d6753
Vladimir Sibirov <trustmaster@kodigy.com>
2018-11-08 22:15:19 +0100

    Add test for long running process

--- component_test.go
+++ component_test.go
@@ -4,16 +4,20 @@ import (
 	"testing"
 )
 
-type doubleOnce struct {
+// This component interface is common for many test cases
+type intInAndOut struct {
 	In <-chan int
 	Out chan<- int
 }
+
+type doubleOnce intInAndOut
 
 func (c *doubleOnce) Process() {
 	i := <-c.In
 	c.Out <- 2*i
 }
 
+// Test a simple component that runs only once
 func TestSimpleComponent(t *testing.T) {
 	in := make(chan int)
 	out := make(chan int)
@@ -34,3 +38,39 @@ func TestSimpleComponent(t *testing.T) {
 	<-wait
 }
 
+type doubler intInAndOut
+
+func (c *doubler) Process() {
+	for i := range c.In {
+		c.Out <- 2*i
+	}
+}
+
+func TestSimpleLongRunningComponent(t *testing.T) {
+	data := map[int]int{
+		12: 24,
+		7: 14,
+		400: 800,
+	}
+	in := make(chan int)
+	out := make(chan int)
+	c := &doubler{
+		in,
+		out,
+	}
+
+	wait := Run(c)
+
+	for src, expected := range data {
+		in <- src
+		actual := <- out
+
+		if actual != expected {
+			t.Errorf("%d != %d", actual, expected)
+		}
+	}
+
+	// We have to close input for the process to finish
+	close(in)
+	<-wait
+}
