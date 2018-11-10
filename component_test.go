ef2f6709a9ee3e7736a2ebfca11daa18a14d616a
Vladimir Sibirov <trustmaster@kodigy.com>
2018-11-10 21:18:25 +0100

    Add a more sophisticated test

--- component_test.go
+++ component_test.go
@@ -46,6 +46,7 @@ func (c *doubler) Process() {
 	}
 }
 
+// Test a simple long running component with one input
 func TestSimpleLongRunningComponent(t *testing.T) {
 	data := map[int]int{
 		12: 24,
@@ -72,5 +73,95 @@ func TestSimpleLongRunningComponent(t *testing.T) {
 
 	// We have to close input for the process to finish
 	close(in)
+	<-wait
+}
+
+// A component with two inputs and one output
+type adder struct {
+	Op1 <-chan int
+	Op2 <-chan int
+	Sum chan<- int
+}
+
+func (c *adder) Process() {
+	op1Buf := make([]int, 0, 10)
+	op2Buf := make([]int, 0, 10)
+	addOp := func(op int, buf, otherBuf *[]int) {
+		if len(*otherBuf) > 0 {
+			otherOp := (*otherBuf)[0]
+			*otherBuf = (*otherBuf)[1:]
+			c.Sum <- (op + otherOp)
+		} else {
+			*buf = append(*buf, op)
+		}
+	}
+
+	const inputCount = 2
+	closed := make([]struct{}, 0, inputCount)
+	completed := func() bool {
+		closed = append(closed, struct{}{})
+		return len(closed) >= inputCount
+	}
+
+	closeOuts := func() {
+		close(c.Sum)
+	}
+
+	defer closeOuts()
+
+	for {
+		select {
+		case op1, ok := <-c.Op1:
+			if ok {
+				addOp(op1, &op1Buf, &op2Buf)
+			} else if completed() {
+				return
+			}
+			
+		case op2, ok := <-c.Op2:
+			if ok {
+				addOp(op2, &op2Buf, &op1Buf)
+			} else if completed() {
+				return
+			}
+		}
+	}
+}
+
+func TestComponentWithTwoInputs(t *testing.T) {
+	op1 := []int{3, 5, 92, 28}
+	op2 := []int{38, 94, 4, 9}
+	sums := []int{41, 99, 96, 37}
+
+	in1 := make(chan int)
+	in2 := make(chan int)
+	out := make(chan int)
+	c := &adder{in1, in2, out}
+
+	wait := Run(c)
+
+	go func() {
+		for _, n := range op1 {
+			in1 <- n
+		}
+		close(in1)
+	}()
+
+	go func() {
+		for _, n := range op2 {
+			in2 <- n
+		}
+		close(in2)
+	}()
+	
+	i := 0
+	for actual := range out {
+		expected := sums[i]
+		if actual != expected {
+			t.Errorf("%d != %d", actual, expected)
+		}
+		i++
+	}
+	
 	<-wait
 }
