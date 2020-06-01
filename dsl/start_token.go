39cc4e567ad4a9e81974dbacc7c0478dfeb92d17
Vladimir Sibirov <trustmaster@kodigy.com>
2020-06-01 22:05:45 +0200

    Add StartToken

---
+++ dsl/start_token.go
@@ -0,0 +1,24 @@
+package dsl
+
+// StartToken starts a stream of tokens from a given file
+type StartToken struct {
+	File <-chan *File
+	Init chan<- Token // Initial token, doesn't need scanning
+	Next chan<- Token // Next token to scan
+}
+
+// Process reads files and starts token streams
+func (s *StartToken) Process() {
+	for f := range s.File {
+		t := Token{
+			Type:  tokNewFile,
+			File:  f,
+			Pos:   0,
+			Value: "",
+		}
+		// Send to Init, which should go directly to Tokenizer output
+		s.Init <- t
+		// Send to Next, which should go to Scanners
+		s.Next <- t
+	}
+}
